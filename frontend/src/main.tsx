b3cc108045d7cc3d816bfbbe9c02173e6d810c76
aarkue <mail@aarkue.eu>
2023-11-13 17:01:09 +0100

    Update toast styling

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -22,7 +22,7 @@ const router = createBrowserRouter([
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Toaster />
+    <Toaster position="bottom-left" />
     <RouterProvider router={router} />
   </React.StrictMode>,
 );
