6caf3f2705425d1284ed7bdc4901f8fb6da48dd2
aarkue <mail@aarkue.eu>
2024-05-10 20:28:14 +0200

    Add NODE_TYPE_SIZE

--- frontend/src/routes/visual-editor/helper/const.ts
+++ frontend/src/routes/visual-editor/helper/const.ts
@@ -5,13 +5,6 @@ import GateNode from "./node/GateNode";
 export const EVENT_TYPE_LINK_TYPE = "eventTypeLink";
 export const EVENT_TYPE_NODE_TYPE = "eventType";
 export const GATE_NODE_TYPE = "gate";
-export const GATE_LINK_TYPE = "gateLink";
-
-export const CONSTRAINT_TYPES = [
-  "response",
-  "unary-response",
-  "non-response",
-] as const;
 
 export const nodeTypes = {
   [EVENT_TYPE_NODE_TYPE]: EventTypeNode,
@@ -19,4 +12,9 @@ export const nodeTypes = {
 };
 export const edgeTypes = {
   [EVENT_TYPE_LINK_TYPE]: EventTypeLink,
+};
+
+export const NODE_TYPE_SIZE = {
+  [EVENT_TYPE_NODE_TYPE]: { width: 240, minHeight: 110.58 },
+  [GATE_NODE_TYPE]: { width: 128, minHeight: 80 },
 };
