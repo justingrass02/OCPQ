716e3e55fc6852dec5962948e25f1a8560a180ce
aarkue <mail@aarkue.eu>
2024-08-28 23:39:12 +0200

    Release v0.4.1

--- tauri/tailwind.config.js
+++ tauri/tailwind.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   darkMode: ["class"],
   content: [
-    "../frontend/**/*.{ts,tsx}",
+    "../frontend/src/**/*.{ts,tsx}",
     "./components/**/*.{ts,tsx}",
     "./app/**/*.{ts,tsx}",
     "./src/**/*.{ts,tsx}",
